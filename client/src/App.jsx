c969fbfef35e35199d63f3675a9c34667a115af5
Travis Ryan Bunn <57367177+drtravis4@users.noreply.github.com>
2023-04-09 15:27:37 -0500

    added items to header and added logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,42 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
-    <div className="bg-red-400">testing</div>
-  )
+    <div>
+      <header className="p-4 flex justify-between">
+        <a href="" className="flex item-center gap-1">
+          <img
+            src="https://nashvilleluggagestorage.com/wp-content/uploads/2022/06/LuggageDrop_DuoTone_Logo_500x200.svg"
+            alt="logo"
+            width="100"
+            height="300"
+          />
+          <span className="font-bold text-xl">Luggage Drop</span>
+        </a>
+        <div className="flex">
+          <div>Location</div>
+          <div>Day</div>
+          <div>Items</div>
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+              />
+            </svg>
+          </button>
+        </div>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
