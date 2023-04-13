e3e2d2089f4f5dab9829101109b8406a3ba85152
Travis Ryan Bunn <57367177+drtravis4@users.noreply.github.com>
2023-04-12 22:56:30 -0500

    touhcing up header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,14 +19,15 @@ function App() {
           <div>Day</div>
           <div className="border-l border-gray-300"></div>
           <div>Items</div>
-          <button className="bg-primary text-white rounded-full">
+          {/* Search Icon */}
+          <button className="bg-primary text-white p-1 rounded-full">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="w-6 h-6"
+              className="w-4 h-4"
             >
               <path
                 strokeLinecap="round"
@@ -35,6 +36,17 @@ function App() {
               />
             </svg>
           </button>
+        </div>
+        {/* User Widget */}
+        <div className="gap-2 flex border border-gray-300 rounded-full py-2 px-4">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+</svg>
+
+
         </div>
       </header>
     </div>
